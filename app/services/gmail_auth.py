73c49cca3996350d7e79fb1661d667ece4e8730c
Ahmed kasem <ahmedabouelkasem72@gmail.com>
2026-08-09 01:34:52 +0300

    Add Gmail authentication service implementation

---
+++ app/services/gmail_auth.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+
+SCOPES = [
+    "https://www.googleapis.com/auth/gmail.modify"
+]
+
+BASE_DIR = Path(__file__).resolve().parents[2]
+
+CREDENTIALS_PATH = BASE_DIR / "credentials.json"
+
+TOKEN_PATH = BASE_DIR / "token.json"
+
+def authenticate():
+    creds = None
+
+    if TOKEN_PATH.exists():
+        creds = Credentials.from_authorized_user_file(
+            TOKEN_PATH,
+            SCOPES
+        )
+
+    if not creds or not creds.valid:
+
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                CREDENTIALS_PATH,
+                SCOPES
+            )
+
+            creds = flow.run_local_server(port=0)
+
+        with open(TOKEN_PATH, "w") as token:
+            token.write(creds.to_json())
+
+    service = build(
+        "gmail",
+        "v1",
+        credentials=creds
+    )
+
+    return service
